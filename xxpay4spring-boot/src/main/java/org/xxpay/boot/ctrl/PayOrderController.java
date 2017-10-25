1e1de0d63061be9e0694669dd5da27cd032e0016
jmdhappy <jmdhappy>
2017-10-25 11:37:08 +0800

    优化boot版本代码结构

--- xxpay4spring-boot/src/main/java/org/xxpay/boot/ctrl/PayOrderController.java
+++ xxpay4spring-boot/src/main/java/org/xxpay/boot/ctrl/PayOrderController.java
@@ -251,18 +251,4 @@ public class PayOrderController {
         return payOrder;
     }
 
-    String getJsonParam(String[] names, Object[] values) {
-        JSONObject jsonParam = new JSONObject();
-        for (int i = 0; i < names.length; i++) {
-            jsonParam.put(names[i], values[i]);
-        }
-        return jsonParam.toJSONString();
-    }
-
-    String getJsonParam(String name, Object value) {
-        JSONObject jsonParam = new JSONObject();
-        jsonParam.put(name, value);
-        return jsonParam.toJSONString();
-    }
-
 }
