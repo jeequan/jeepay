570c483174857075e94d200d900452e942f64cda
jmdhappy@126.com <jmdhappy@126.com>
2019-01-09 11:39:25 +0800

    修改微信通知验证bug

--- xxpay4spring-cloud/xxpay-service/src/main/java/org/xxpay/service/controller/Notify4WxPayController.java
+++ xxpay4spring-cloud/xxpay-service/src/main/java/org/xxpay/service/controller/Notify4WxPayController.java
@@ -114,7 +114,7 @@ public class Notify4WxPayController extends Notify4BasePay {
 
 		//校验结果是否成功
 		if (!PayConstant.RETURN_VALUE_SUCCESS.equalsIgnoreCase(params.getResultCode())
-				|| !PayConstant.RETURN_VALUE_SUCCESS.equalsIgnoreCase(params.getResultCode())) {
+				|| !PayConstant.RETURN_VALUE_SUCCESS.equalsIgnoreCase(params.getReturnCode())) {
 			_log.error("returnCode={},resultCode={},errCode={},errCodeDes={}", params.getReturnCode(), params.getResultCode(), params.getErrCode(), params.getErrCodeDes());
 			payContext.put("retMsg", "notify data failed");
 			return false;
