bbd521731bb5cd70a5182f06eda9ed427fcc6747
dingzhiwei <jmdhappy@126.com>
2022-03-26 14:26:24 +0800

    修复paypal支付查询问题

--- jeepay-payment/src/main/java/com/jeequan/jeepay/pay/channel/pppay/PppayPayOrderQueryService.java
+++ jeepay-payment/src/main/java/com/jeequan/jeepay/pay/channel/pppay/PppayPayOrderQueryService.java
@@ -5,6 +5,8 @@ import com.jeequan.jeepay.core.entity.PayOrder;
 import com.jeequan.jeepay.pay.channel.IPayOrderQueryService;
 import com.jeequan.jeepay.pay.model.MchAppConfigContext;
 import com.jeequan.jeepay.pay.rqrs.msg.ChannelRetMsg;
+import com.jeequan.jeepay.pay.service.ConfigContextQueryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,13 +18,17 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class PppayPayOrderQueryService implements IPayOrderQueryService {
+
     @Override
     public String getIfCode() {
         return CS.IF_CODE.PPPAY;
     }
 
+    @Autowired
+    private ConfigContextQueryService configContextQueryService;
+
     @Override
     public ChannelRetMsg query(PayOrder payOrder, MchAppConfigContext mchAppConfigContext) throws Exception {
-        return mchAppConfigContext.getPaypalWrapper().processOrder(null, payOrder);
+        return configContextQueryService.getPaypalWrapper(mchAppConfigContext).processOrder(null, payOrder);
     }
 }
