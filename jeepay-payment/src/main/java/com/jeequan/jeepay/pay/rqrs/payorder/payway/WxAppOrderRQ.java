9c8b13beded928d021878175a58558708d058a98
xiaoyu <17601026937@163.com>
2023-02-15 14:15:33 +0800

    支付宝、微信APP RQ修改

--- jeepay-payment/src/main/java/com/jeequan/jeepay/pay/rqrs/payorder/payway/WxAppOrderRQ.java
+++ jeepay-payment/src/main/java/com/jeequan/jeepay/pay/rqrs/payorder/payway/WxAppOrderRQ.java
@@ -22,26 +22,27 @@ import lombok.Data;
 import javax.validation.constraints.NotBlank;
 
 /*
-* 支付方式： ALI_APP
-*
-* @author terrfly
-* @site https://www.jeequan.com
-* @date 2021/6/8 17:34
-*/
+ *  支付方式： WX_APP
+ *
+ * @author xiaoyu
+ * @site https://www.jeequan.com
+ * @date 2022/12/20 8:12
+ */
 @Data
-public class AliAppOrderRQ extends UnifiedOrderRQ {
+public class WxAppOrderRQ extends UnifiedOrderRQ {
 
-    /** 支付宝用户ID **/
-    @NotBlank(message = "用户ID不能为空")
-    private String buyerUserId;
+    /** 微信openid **/
+    @NotBlank(message = "openid不能为空")
+    private String openid;
 
     /** 构造函数 **/
-    public AliAppOrderRQ(){
-        this.setWayCode(CS.PAY_WAY_CODE.ALI_APP); //默认 wayCode, 避免validate出现问题
+    public WxAppOrderRQ(){
+        this.setWayCode(CS.PAY_DATA_TYPE.WX_APP); //默认 wayCode, 避免validate出现问题
     }
+
 
     @Override
     public String getChannelUserId(){
-        return this.buyerUserId;
+        return this.openid;
     }
 }
