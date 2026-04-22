5469f3da4655099acd48531599ec1732dc2823f9
大森林 <aquan-dev@local>
2026-04-22 21:30:27 +0800

    chore(release): 撞版本号到 V3.2.8

--- docs/install/config.sh
+++ docs/install/config.sh
@@ -31,7 +31,7 @@ mysql_pwd="jeepaydb123456"
 # 若使用自行构建的 arm64 RocketMQ 镜像，请同时覆盖 rocketmqImage 并将下行改为 linux/arm64：
 # rocketmqPlatform="linux/amd64"
 
-# 源码 ref：install.sh 默认锁到 V3.2.7 release tag。如需切到其他 tag 或 master，
+# 源码 ref：install.sh 默认锁到 V3.2.8 release tag。如需切到其他 tag 或 master，
 # 取消以下注释并改为目标 ref；确保该 ref 下的 SQL / broker.conf.template / nginx.conf /
 # conf/* 与所选业务镜像版本兼容。
 # jeepayRef="master"
